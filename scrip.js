84870fc8cbcece21a1a6da12dc168e49f9154d3c
RafiDevari <rafidevari2004@gmail.com>
2024-03-01 23:47:37 +0700

    bang udah bang

---
+++ scrip.js
@@ -0,0 +1,12 @@
+function project() {
+    var x = document.getElementById("loli2");
+    x.style.display = "block";
+    x.classList.add("project");
+
+}
+
+function skill() {
+    var x = document.getElementById("loli1");
+    x.style.display = "block";
+    x.classList.add("project");
+}
