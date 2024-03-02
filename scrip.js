533b3539f385283dea7cb77b7cea549198b2b26a
RafiDevari <rafidevari2004@gmail.com>
2024-03-02 14:28:01 +0700

    meninggal

--- scrip.js
+++ scrip.js
@@ -1,12 +1,22 @@
 function project() {
     var x = document.getElementById("loli2");
+    var y = document.getElementById("loli1");
     x.style.display = "block";
+    y.style.display="none";
     x.classList.add("project");
+
 
 }
 
 function skill() {
     var x = document.getElementById("loli1");
+    var y = document.getElementById("loli2");
     x.style.display = "block";
+    y.style.display="none";
     x.classList.add("project");
+}
+
+
+function contact(){
+    
 }
